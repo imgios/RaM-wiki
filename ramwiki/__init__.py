f1f57bbbc579cff11b46d44cdbae8ad4db7f8040
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-08-28 12:55:47 +0200

    Update init script

--- ramwiki/__init__.py
+++ ramwiki/__init__.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 # a simple page that shows the status
 @app.route('/status')
-def hello():
-    return 'Running!'
+def status():
+    return render_template('status.html')
