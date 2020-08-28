d636f7a631136339c40d19a4e4e7ecbefdf04522
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-08-28 17:03:03 +0200

    Add getEntityCount function

--- ramwiki/utilities.py
+++ ramwiki/utilities.py
@@ -26,3 +26,22 @@ def getRelationshipsCount(dbGraph):
         logger.warning("dbGraph not initialized: cannot retrieve nodes count.")
     else:
         return relsCount
+
+def read_query_from_file(cypher_file_path):
+    with open(cypher_file_path, 'r') as cypher_file:
+        cypher_query = cypher_file.read()
+    return cypher_query
+
+def getEntityCount(dbGraph, entity):
+    # c = character, e = episode, l = location
+    if entity == "c":
+        query = read_query_from_file('cyphers/queries/getCharactersCount.cyp')
+    elif entity == "e":
+        query = read_query_from_file('cyphers/queries/getEpisodesCount.cyp')
+    elif entity == "l":
+        query = read_query_from_file('cyphers/queries/getLocationsCount.cyp')
+    else:
+        logger.warning("Entity not defined.")
+    
+    cursor = dbGraph.run(query)
+    return cursor.data()[0]['count']
