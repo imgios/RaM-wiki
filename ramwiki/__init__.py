d67834eefa8e12c5b49e69022d175b4c24d3dc52
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-08-28 14:00:11 +0200

    Update status template vars in the init script
    TO-DO: Retrieve those variables doing some queries.

--- ramwiki/__init__.py
+++ ramwiki/__init__.py
@@ -7,4 +7,9 @@ app.secret_key = os.urandom(24)
 # a simple page that shows the status
 @app.route('/status')
 def status():
-    return render_template('status.html')
+    return render_template('status.html',
+        number_of_characters='TO-DO',
+        number_of_episodes='TO-DO',
+        number_of_locations='TO-DO',
+        number_of_relationships='TO-DO',
+        number_of_nodes='TO-DO')
