519ce39533feb02b8ae2ee014851d4b5a7731487
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-08-28 16:14:30 +0200

    Update with number of nodes and relationships

--- ramwiki/__init__.py
+++ ramwiki/__init__.py
@@ -1,15 +1,22 @@
 import os
+from ramwiki.utilities import getConnection, getNodesCount, getRelationshipsCount
 from flask import Flask, render_template
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+# TO-DO: register blueprints
+
 # a simple page that shows the status
 @app.route('/status')
 def status():
+    dbGraph = getConnection()
+    nodesCount = getNodesCount(dbGraph)
+    relsCount = getRelationshipsCount(dbGraph)
+
     return render_template('status.html',
         number_of_characters='TO-DO',
         number_of_episodes='TO-DO',
         number_of_locations='TO-DO',
-        number_of_relationships='TO-DO',
-        number_of_nodes='TO-DO')
+        number_of_relationships=relsCount,
+        number_of_nodes=nodesCount)
