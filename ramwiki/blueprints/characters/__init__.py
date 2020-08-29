1fa56878e936f9a0967688d0f4a83a55055201ef
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-08-29 11:38:42 +0200

    Define statically two characters index vars

--- ramwiki/blueprints/characters/__init__.py
+++ ramwiki/blueprints/characters/__init__.py
@@ -8,4 +8,6 @@ characters = Blueprint(
 
 @characters.route('/')
 def index():
-    return render_template('characters/index.html')
+    return render_template('characters/index.html',
+        number_of_characters="1", # testing purposed, must be changed
+        characters = [{"name":"Test", "status":"alive", "species":"ai"},{"name":"Test2", "status":"alive", "species":"ai"},{"name":"Test3", "status":"alive", "species":"ai"}]) # testing purposed, must be changed
