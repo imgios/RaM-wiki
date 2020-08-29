66b0a111c10b9664a47548bcb1a85ed6a514c0df
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-08-29 11:03:04 +0200

    Add Characters blueprint

--- ramwiki/blueprints/characters/__init__.py
+++ ramwiki/blueprints/characters/__init__.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 characters = Blueprint(
     'characters',
     __name__,
     template_folder='templates'
 )
+
+@characters.route('/')
+def index():
+    return render_template('characters/index.html')
