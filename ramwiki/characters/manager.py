011bc1f1878aa163e3f298eeaceaeb7d1681f532
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-09-05 12:17:23 +0200

    Add getCharacterByNumber function

--- ramwiki/characters/manager.py
+++ ramwiki/characters/manager.py
@@ -7,7 +7,12 @@ class CharacterManager:
 
     def getByName(self, characterName):
         query = read_query_from_file('ramwiki/characters/queries/getCharacterByName.cyp')
-        cursor = self.graph.run(query, characterName = characterName)
+        cursor = self.graph.run(query, paramCharacterName = characterName)
+        return {"data": cursor.data()}
+
+    def getByNumber(self, characterNumber):
+        query = read_query_from_file('ramwiki/characters/queries/getCharacterByNo.cyp')
+        cursor = self.graph.run(query, paramCharacterNumber = characterNumber)
         return {"data": cursor.data()}
 
     #TO-DO: Add more function in order to retrive different data
