53a8b580036e653d3c34b615a4fd247399514198
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-09-05 12:36:25 +0200

    Update query required param

--- ramwiki/characters/manager.py
+++ ramwiki/characters/manager.py
@@ -7,12 +7,12 @@ class CharacterManager:
 
     def getByName(self, characterName):
         query = read_query_from_file('ramwiki/characters/queries/getCharacterByName.cyp')
-        cursor = self.graph.run(query, paramCharacterName = characterName)
+        cursor = self.graph.run(query, character_name = characterName)
         return {"data": cursor.data()}
 
     def getByNumber(self, characterNumber):
         query = read_query_from_file('ramwiki/characters/queries/getCharacterByNo.cyp')
-        cursor = self.graph.run(query, paramCharacterNumber = characterNumber)
+        cursor = self.graph.run(query, character_number = characterNumber)
         return {"data": cursor.data()}
 
     #TO-DO: Add more function in order to retrive different data
