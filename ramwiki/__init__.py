dd04752b61ddf420f096969480252bb946948f69
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-09-07 17:09:30 +0200

    Add index route that redirect to the status page

--- ramwiki/__init__.py
+++ ramwiki/__init__.py
@@ -1,6 +1,6 @@
 import os
 from ramwiki.utilities import getConnection, getNodesCount, getRelationshipsCount, getEntityCount
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from .characters import characters
 
 app = Flask(__name__)
@@ -8,6 +8,11 @@ app.secret_key = os.urandom(24)
 
 # TO-DO: register blueprints
 app.register_blueprint(characters, url_prefix='/characters')
+
+# (?) TEMP: App index redirect to the status page
+@app.route('/')
+def index():
+    return redirect(url_for('status'), code = 307)
 
 # a simple page that shows the status
 @app.route('/status')
