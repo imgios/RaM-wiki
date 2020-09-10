01925c208d37ea537e7d455a1fea773e6d80ab16
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-09-10 20:49:29 +0200

    Add episodes.index route

--- ramwiki/episodes/__init__.py
+++ ramwiki/episodes/__init__.py
@@ -1,7 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from .manager import EpisodeManager
 
 episodes = Blueprint (
     'episodes',
     __name__,
     template_folder='templates'
 )
+
+@episodes.route('/')
+@episodes.route('/index')
+def index():
+    episodeManager = EpisodeManager()
+    season = {}
+    for i in range(1, 5): # [1, 5[
+        result = episodeManager.getBySeason(i)
+        season[str(i)] = len(result['data'])
+    
+    return render_template('episodes/index.html',
+        season_episodes_number = season)
