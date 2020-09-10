57752c4e8c551fcb783637533296925dd76d4bab
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-09-10 17:35:55 +0200

    Remove unused import from character init

--- ramwiki/characters/__init__.py
+++ ramwiki/characters/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request
 from .manager import CharacterManager
 from py2neo import Node
 from ..utilities import getEntityCount
